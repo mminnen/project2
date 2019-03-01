d9b7a671991b914a1ae50595f4516e986c8773ad
martijn.minnen <martijn.minnen@dimensiondata.com>
2019-03-01 06:48:40 -0800

    Username, Channels and layout working

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
-# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 my_channels = {"Main Channel"}
-# messages = ...
+messages = ["test1", "test2"]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", my_channels=my_channels)
 
-@app.route("/rooms")
-def rooms():
-    return render_template("rooms.html", my_channels=my_channels)
 
 @app.route("/channels", methods=['POST'])
 def channels():
     new_channel = request.form.get('new_channel')
     my_channels.add(new_channel)
-    return redirect(url_for("rooms"))
+    return redirect(url_for("index"))
 
-@app.route("/messages", methods=['GET','POST'])
+
+@app.route("/messages", methods=['GET', 'POST'])
 def messages():
     if request.method == 'GET':
-        messages = ["test1", "test2"]
-        return jsonify({"user": "User", "date": "xx-xx-xxxx", "messages":messages})
+                return jsonify({"user": "User", "date": "xx-xx-xxxx", "messages": messages})
+
+
+if __name__ == '__main__':
+    socketio.run(app)
--- static/username.js
+++ static/username.js
@@ -1,40 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-                // By default, submit button is disabled
-                document.querySelector('#submit').disabled = true;
+    // localStorage.clear();
 
-                document.querySelector('#welcome_message').innerHTML = `Pick a username:`;
+        if (localStorage.getItem('user')) {
+            let username = localStorage.getItem('user');
+            document.querySelector('#welcome_message').innerHTML = `Welcome back ${username}!`;
+        }
 
-                // Enable button only if there is text in the input field
-                document.querySelector('#username').onkeyup = () => {
-                    if (document.querySelector('#username').value.length > 0)
-                        document.querySelector('#submit').disabled = false;
-                    else
-                        document.querySelector('#submit').disabled = true;
-                };
+        else {
 
-                    if (localStorage.getItem('user'))
-				       {
-			        //  let username = localStorage.getItem('username');
-                        let username = localStorage.getItem('user');
-                        document.querySelector('#welcome_message').innerHTML = `Welcome back ${username}!`;
-				        }
 
-                    document.querySelector('#form').onsubmit = () => {
-
-                    // Say hello to the user
-                    let username = document.querySelector('#username').value;
-                    // Store username in local storage
-                    localStorage.setItem('user', username);
-                    document.querySelector('#welcome_message').innerHTML = `Hello ${username}!`;
-
-                    // Clear input field and disable button again
-                    document.querySelector('#username').value = '';
-                    document.querySelector('#submit').disabled = true;
-
-                    // Stop form from submitting
-                    return false;
-                };
-
+            $("#prompt_username").modal({
+                backdrop: 'static',
+                keyboard: false
             });
 
+            // By default, submit button is disabled
+            document.querySelector('#submit_name').disabled = true;
+
+            document.querySelector('#welcome_message').innerHTML = `Pick a username!`;
+
+            // Enable button only if there is text in the input field
+            document.querySelector('#username').onkeyup = () => {
+
+                if (document.querySelector('#username').value.length > 2 && document.querySelector('#username').value.length < 12)
+                    document.querySelector('#submit_name').disabled = false;
+                else
+                    document.querySelector('#submit_name').disabled = true;
+            }
+
+
+        document.querySelector('#input_name').onsubmit = () => {
+
+            // Say hello to the user
+            let username = document.querySelector('#username').value;
+            // Store username in local storage
+            localStorage.setItem('user', username);
+            document.querySelector('#welcome_message').innerHTML = `Hello ${username}!`;
+            $("#prompt_username").modal('hide');
+            // Stop form from submitting
+            return false;
+        }
+
+
+        }
+
+
+    });
